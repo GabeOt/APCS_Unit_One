31a21b1d876903b77a6de2bea932ea74e3f826ea
GabeOt <gabriel.otridge@ssfs.org>
2021-09-10 11:54:51 -0400

    I attempted the exercises

--- src/DateFormat.java
+++ src/DateFormat.java
@@ -3,6 +3,19 @@ import java.util.Scanner;
 public class DateFormat {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the current month?");
+        String month = scan.nextLine();
+        System.out.println("What is the current day of the week?");
+        String day = scan.nextLine();
+        System.out.println("What is the current year?");
+        int year = scan.nextInt();
+        System.out.println("What is the day of the month");
+        int Month2 = scan.nextInt();
+        System.out.println("American Format:");
+        System.out.println("month + " " + day + " " + year + " " + Month2");
+
+
 
     }
 }
--- src/MetaJava.java
+++ src/MetaJava.java
@@ -3,7 +3,7 @@ public class MetaJava {
     public static void main(String[] args) {
         System.out.println("public class Hello { ");
         System.out.println("    public static void main(String[] args) { ");
-        System.out.print("        System.out.println("Hello, world!"); ");
+        System.out.print("        System.out.println(\"Hello, world!\"); ");
 
     }
 }
